597b57c8f26a32813db08fc6efdd86ca4aa03e1a
Angus Lynch <alyn649@aucklanduni.ac.nz>
2021-08-25 13:13:56 +1200

    Initial Commit
    Starting development of a hexagonal data structure type.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HexagonDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
